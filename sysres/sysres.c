4d0882cf265026bd9a49996b897117adfdd64783
stesen <stesenchina@gmail.com>
2011-03-14 14:26:26 +0800

    improve sysres.c

--- sysres/sysres.c
+++ sysres/sysres.c
@@ -4,7 +4,11 @@
 #include <unistd.h>
 #include <time.h>
 #include <sys/time.h>
-#include <mpd/client.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 
 #define isspace(c) ((((c) == ' ') || (((unsigned int)((c) - 9)) <= (13 - 9))))
 #define SCAN(match, name) \
@@ -12,7 +16,6 @@
     if (p) { name = p; continue; }
 
 #define LONG_BUFF_SIZE  32
-#define MID_BUFF_SIZE  24
 #define SHORT_BUFF_SIZE 8
 
 #define MPD_IP "127.0.0.1"
@@ -28,28 +31,73 @@ struct cpu_info
 
 int g_cpu_used;
 int cpu_num;
-char mpc_buff[LONG_BUFF_SIZE] = "";
+char mpd_buff[LONG_BUFF_SIZE] = "";
 char ldavg_buff[LONG_BUFF_SIZE] = "";
-char temp_buff[SHORT_BUFF_SIZE] = "";
 char net_buff[LONG_BUFF_SIZE] = "";
 char clk_buff[LONG_BUFF_SIZE] = "";
-char bat_buff[SHORT_BUFF_SIZE] = "";
 
-void mpc(void)
+void mpd(void)
 {
-    FILE *fp;
+    int sock_fd;
+    char sock_buff[256];
+    char stat_buf[8];
+    struct sockaddr_in sin;
+    char *p, *ptr;
     int i;
 
-    fp = popen("mpc current" ,"r");
-    fgets(mpc_buff , sizeof(mpc_buff) , fp);
-    for (i = 0; i < LONG_BUFF_SIZE; i++) {
-        if(mpc_buff[i] == '\n') {
-            mpc_buff[i] = '\0';
-            break;
-        }
+    sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock_fd < 0) {
+        printf("socket err\n");
+        exit(1);
     }
 
-    pclose(fp);
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(MPD_PORT);
+    sin.sin_addr.s_addr = inet_addr(MPD_IP);
+
+    if(connect(sock_fd, (const struct sockaddr *)&sin, sizeof(sin)) == -1) {
+        strcpy(mpd_buff, "connect err\n");
+        return;
+    }
+    recv(sock_fd, sock_buff, sizeof(sock_buff), 0);
+
+    send(sock_fd, "status\n", 7, 0);
+    recv(sock_fd, sock_buff, sizeof(sock_buff), 0);
+
+    p = strstr(sock_buff, "state");
+    if(p == NULL) {
+        strcpy(mpd_buff, "read status err\n");
+        return;
+    }
+    p += sizeof("state: ") - 1;
+    i = 0;
+    while(*p != '\n') {
+        stat_buf[i] = *p;
+        p++;
+        i++;
+    }
+    stat_buf[i] = '\0';
+    if(strcmp(stat_buf, "play") == 0) {
+        send(sock_fd, "currentsong\n", 12, 0);
+        recv(sock_fd, sock_buff, sizeof(sock_buff), 0);
+        p = strstr(sock_buff, "file");
+        ptr = strrchr(p, '/');
+        if(ptr == NULL) {
+            strcpy(mpd_buff, "read file err\n");
+            return;
+        }
+
+        ptr++;
+        i = 0;
+        while(*ptr != '\n') {
+            mpd_buff[i] = *ptr;
+            ptr++;
+            i++;
+        }
+        mpd_buff[i] = '\0';
+    } else if ((strcmp(stat_buf, "pause") == 0) || (strcmp(stat_buf, "stop") == 0)) {
+        strcpy(mpd_buff, stat_buf);
+    }
 }
 
 void cal_cpu_info(struct cpu_info *o, struct cpu_info *n)
@@ -61,7 +109,7 @@ void cal_cpu_info(struct cpu_info *o, struct cpu_info *n)
     nd = n->user + n->nice + n->system +n->idle;
     id = n->user - o->user;
     sd = n->system - o->system;
-    g_cpu_used = (((sd + id) * 100.0) / (nd - od));
+    g_cpu_used = (((sd + id) * 99.0) / (nd - od));
 }
 
 
@@ -162,6 +210,8 @@ int men(void)
         char *str[4];
     } z;
 
+    bzero(&z, sizeof(z));
+
     fp = fopen("/proc/meminfo", "r");
     while (fgets(linebuf, sizeof(linebuf), fp))
     {
@@ -208,71 +258,106 @@ void loadavg(void)
     fclose(fp);
 }
 
-void temp(void)
+int temp(void)
 {
     FILE *fp;
-    int i;
+    int n;
+    char tmp_buff[SHORT_BUFF_SIZE];
 
-    fp = popen("acpi -t|cut -d\" \" -f4" ,"r");
-    fgets(temp_buff , sizeof(temp_buff) , fp);
+    fp = fopen("/sys/class/thermal/thermal_zone0/temp", "r");
+    fgets(tmp_buff, sizeof(tmp_buff), fp);
+    n = atoi(tmp_buff);
 
-    for(i = 0; i < SHORT_BUFF_SIZE; i++) {
-        if(temp_buff[i] == '.') {
-            temp_buff[i] = '\0';
-            break;
-        }
-    }
-
-    pclose(fp);
+    fclose(fp);
+    return (n / 1000);
 }
 
 void net(void)
 {
     FILE *fp;
-    int i, j, k;
+    int i;
     char *p;
-    char buf[64];
+    char buf[256];
+    unsigned int recved1[2], sent1[2];
+    unsigned int recved2[2], sent2[2];
 
-    fp = popen("ifstat -i eth0,wlan0 -q 1 1" ,"r");
-    fgets(buf , sizeof(buf) , fp);
-    fgets(buf , sizeof(buf) , fp);
-    fgets(buf , sizeof(buf) , fp);
+    fp = fopen("/proc/net/dev" ,"r");
+    fgets(buf, sizeof(buf), fp);
+    fgets(buf, sizeof(buf), fp);
 
-    j = k = 0;
-    p = buf;
+    while(fgets(buf , sizeof(buf) , fp)) {
+        p = buf;
+        p = skip_whitespace(p);
+        if((strncmp(p, "eth", 3)) == 0) {
+            p = skip_non_whitespace(p);
+            p = skip_whitespace(p);
 
-    for(i = 0; i < LONG_BUFF_SIZE; i++) {
-        if(!(*p)) {
-            break;
-        }
+            sscanf(p, "%u", &recved1[0]);
 
-        if((*p <= '9') && (*p >= '0')) {
-            net_buff[k] = *p;
-            k++;
-        } else if(*p == '.') {
-            p += 2;
-            switch(j) {
-                case 0:
-                case 2:
-                    net_buff[k] = '/';
-                    k++;
-                    break;
-                case 1:
-                    net_buff[k] = ' ';
-                    k++;
-                    break;
-                case 3:
-                    net_buff[k] = '\0';
-                    k++;
-                    goto exitnet;
+            for(i = 0; i < 8; i++) {
+                p = skip_whitespace(p);
+                p = skip_non_whitespace(p);
             }
-            j++;
+            p = skip_whitespace(p);
+
+            sscanf(p, "%u", &sent1[0]);
+        } else if ((strncmp(p, "wlan", 4)) == 0) {
+            p = skip_non_whitespace(p);
+            p = skip_whitespace(p);
+
+            sscanf(p, "%u", &recved1[1]);
+
+            for(i = 0; i < 8; i++) {
+                p = skip_whitespace(p);
+                p = skip_non_whitespace(p);
+            }
+            p = skip_whitespace(p);
+
+            sscanf(p, "%u", &sent1[1]);
         }
-        p++;
+    }
+    sleep(1);
+    fseek(fp, 0, SEEK_SET);
+    fgets(buf, sizeof(buf), fp);
+    fgets(buf, sizeof(buf), fp);
+
+    while(fgets(buf , sizeof(buf) , fp)) {
+        p = buf;
+        p = skip_whitespace(p);
+        if((strncmp(p, "eth", 3)) == 0) {
+            p = skip_non_whitespace(p);
+            p = skip_whitespace(p);
+
+            sscanf(p, "%u", &recved2[0]);
+
+            for(i = 0; i < 8; i++) {
+                p = skip_whitespace(p);
+                p = skip_non_whitespace(p);
+            }
+            p = skip_whitespace(p);
+
+            sscanf(p, "%u", &sent2[0]);
+        } else if ((strncmp(p, "wlan", 4)) == 0) {
+            p = skip_non_whitespace(p);
+            p = skip_whitespace(p);
+
+            sscanf(p, "%u", &recved2[1]);
+
+            for(i = 0; i < 8; i++) {
+                p = skip_whitespace(p);
+                p = skip_non_whitespace(p);
+            }
+            p = skip_whitespace(p);
+            sscanf(p, "%u", &sent2[1]);
+        }
     }
 
-exitnet:
-    pclose(fp);
+    fclose(fp);
+    sprintf(net_buff, "%u/%uK %u/%uK",
+            ((recved2[0] - recved1[0]) >> 10),
+            ((sent2[0] - sent1[0]) >> 10),
+            ((recved2[1] - recved1[1]) >> 10),
+            ((sent2[1] - sent1[1])) >> 10);
 }
 
 void clk(void)
@@ -287,53 +372,60 @@ void clk(void)
 
 int batt(void)
 {
-    FILE *fp;
-    int j;
-    char tmp_buff[128];
-    char *p;
+    FILE *fpi, *fps;
+    char tmp_buf1[LONG_BUFF_SIZE * 2];
+    char tmp_buf2[LONG_BUFF_SIZE * 2];
+    int rem, total;
 
-    fp = popen("acpi -b" ,"r");
-    fgets(tmp_buff , sizeof(tmp_buff) , fp);
-
-    p = tmp_buff;
-    j = 0;
-
-    while ((*p != '\0')) {
-        if((*p == ',') && (*(p + 1) == ' ')) {
-            p += 2;
-            while(*p != '%') {
-                bat_buff[j] = *p;
-                j++;
-                p++;
-            }
-            break;
-        }
-        p++;
-    }
-    bat_buff[j] = '\0';
-
-    pclose(fp);
-
-    if(strcmp(bat_buff, "100") == 0) {
-        return 1;
-    } else {
+    fps = fopen("/proc/acpi/battery/BAT0/state", "r");
+    if(fps == NULL) {
+        fclose(fps);
         return 0;
     }
+    fgets(tmp_buf1, sizeof(tmp_buf1), fps);
+    fgets(tmp_buf1, sizeof(tmp_buf1), fps);
+    fgets(tmp_buf1, sizeof(tmp_buf1), fps);
+
+    if(strcmp(tmp_buf1, "charging state:          charged\n") == 0){
+        fclose(fps);
+        return 0;
+    }
+
+    fpi = fopen("/proc/acpi/battery/BAT0/info", "r");
+    if(fpi == NULL) {
+        fclose(fps);
+        fclose(fpi);
+        return 0;
+    }
+
+    fgets(tmp_buf1, sizeof(tmp_buf1), fps);
+    fgets(tmp_buf1, sizeof(tmp_buf1), fps);
+
+    fgets(tmp_buf2, sizeof(tmp_buf2), fpi);
+    fgets(tmp_buf2, sizeof(tmp_buf2), fpi);
+    fgets(tmp_buf2, sizeof(tmp_buf2), fpi);
+
+
+    sscanf(tmp_buf1, "remaining capacity:      %d mAh", &rem);
+    sscanf(tmp_buf2, "last full capacity:      %d mAh", &total);
+
+    fclose(fps);
+    fclose(fpi);
+    return ((rem * 100) / total);
 }
 
 int main(void)
 {
-    mpc();
+    mpd();
     loadavg();
-    temp();
     net();
     clk();
     int bat = batt();
 
-    if (bat) {
-        printf("音乐.%s | 处理器.%d%%  %s| 内存.%dM | 温度.%s°C | 网络.%s | %s\n", mpc_buff, cpu(), ldavg_buff, men(), temp_buff, net_buff, clk_buff);
+    if (bat == 0) {
+        printf("音乐.%s | 处理器.%d%%  %s| 内存.%dM | 温度.%d°C | 网络.%s | %s\n", mpd_buff, cpu(), ldavg_buff, men(), temp(), net_buff, clk_buff);
     } else {
-        printf("音乐.%s | 处理器.%d%%  %s| 内存.%dM | 温度.%s°C | 电池.%s%% | 网络.%s | %s\n", mpc_buff, cpu(), ldavg_buff, men(), temp_buff, bat_buff, net_buff, clk_buff);
+        printf("音乐.%s | 处理器.%d%%  %s| 内存.%dM | 温度.%d°C | 电池.%d%% | 网络.%s | %s\n", mpd_buff, cpu(), ldavg_buff, men(), temp(), bat, net_buff, clk_buff);
     }
 
     return 0;
